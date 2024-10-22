bdd48ef3463da389e24f8431f104d4c6157da0be
Ramon Oliveira <ramon.oliveira@be220.com>
2024-10-22 12:48:10 -0300

    cpf com mascara, admin e aluno options na pagina login

--- src/app/pages/start/login/login.page.ts
+++ src/app/pages/start/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, Injector, OnInit } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { APP_ROUTES } from 'src/app/constants/routes.const';
 import { BasePage } from 'src/app/core/base-page';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,7 @@ import { BasePage } from 'src/app/core/base-page';
 export class LoginPage extends BasePage implements OnInit {
 
   public loginGroup: FormGroup;
+  private cpfMask = '000.000.000-00';
 
   constructor(public injector: Injector) {
     super(injector);
@@ -22,11 +24,44 @@ export class LoginPage extends BasePage implements OnInit {
 
   private buildForm(): void {
     this.loginGroup = this.fb.group({
-      email: [null, [Validators.required]],
+      emailCpf: [null, [Validators.required]],
       password: [null, [Validators.required]],
     });
 
-    this.loginGroup.patchValue({ email: 'teste@gmail.com', password: '123456789' });
+    this.loginGroup.patchValue({ emailCpf: 'aluno@gmail.com', password: '123456789' });
+  }
+
+  handleChange(e: any): void {
+    if(e.detail.value === 'admin') {
+      this.loginGroup.get('emailCpf').setValue('admin@gmail.com');
+    }
+    else {
+      this.loginGroup.get('emailCpf').setValue('aluno@gmail.com');
+    }
+  }
+
+  onEmailCpfInput(event: any): void {
+    const input = event.detail.value;
+
+    if (this.isCpf(input)) {
+      // Se o valor for um CPF, aplique a máscara
+      this.loginGroup.get('emailCpf').setValue(this.applyCpfMask(input));
+    }
+  }
+
+  private isCpf(value: string): boolean {
+    // Considera CPF se tiver apenas números e pelo menos 11 dígitos
+    const cpfPattern = /^\d{3}\.?\d{3}\.?\d{3}\-?\d{2}$/;
+    return cpfPattern.test(value.replace(/\D/g, ''));
+  }
+
+  private applyCpfMask(value: string): string {
+    const cleanedValue = value.replace(/\D/g, ''); // Remove qualquer caractere não numérico
+    const match = cleanedValue.match(/^(\d{3})(\d{3})(\d{3})(\d{2})$/);
+    if (match) {
+      return `${match[1]}.${match[2]}.${match[3]}-${match[4]}`;
+    }
+    return value; // Retorna o valor sem modificação se não houver correspondência
   }
 
   async login() {
@@ -41,5 +76,4 @@ export class LoginPage extends BasePage implements OnInit {
   register() {
     this.navigateTo([APP_ROUTES.START, APP_ROUTES.REGISTER]);
   }
-
 }
